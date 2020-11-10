950d6feadb017cf0a1b7b99173e366682cdb49fd
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-11-10 19:13:56 +0600

    Share image post ui completed

--- app/src/main/java/hn/techcom/com/hnapp/Fragments/ImagePostFragment.java
+++ app/src/main/java/hn/techcom/com/hnapp/Fragments/ImagePostFragment.java
@@ -7,10 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import hn.techcom.com.hnapp.R;
 
 public class ImagePostFragment extends Fragment {
+
+    private Spinner spinner;
 
     public ImagePostFragment() {
         // Required empty public constructor
@@ -26,7 +30,23 @@ public class ImagePostFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_image_post, container, false);
+
+        spinner = view.findViewById(R.id.spinner_post_type);
+
+        String [] arrayPostType = new String[]{"Random",
+                "Positive Thoughts",
+                "Talent",
+                "Culture",
+                "News",
+                "Emergency"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_dropdown_item, arrayPostType);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_post, container, false);
+        return view;
     }
 }
