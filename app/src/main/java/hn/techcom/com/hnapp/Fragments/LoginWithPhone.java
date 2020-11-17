275040500f6b1dfbeb86c805c9bbca8d6626ed29
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-11-17 17:43:39 +0600

    LoginWithPhone.java | added

---
+++ app/src/main/java/hn/techcom/com/hnapp/Fragments/LoginWithPhone.java
@@ -0,0 +1,32 @@
+package hn.techcom.com.hnapp.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hn.techcom.com.hnapp.R;
+
+public class LoginWithPhone extends Fragment {
+
+    public LoginWithPhone() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login_with_phone, container, false);
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
