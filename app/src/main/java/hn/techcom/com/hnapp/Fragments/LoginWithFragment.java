b493df9f8d52f47b1f14dd897e3b4becadadc554
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-11-17 16:02:11 +0600

    LoginWithFragment.java |  google sign-in added

--- app/src/main/java/hn/techcom/com/hnapp/Fragments/LoginWithFragment.java
+++ app/src/main/java/hn/techcom/com/hnapp/Fragments/LoginWithFragment.java
@@ -1,16 +1,35 @@
 package hn.techcom.com.hnapp.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.Objects;
+
 import hn.techcom.com.hnapp.R;
 
-public class LoginWithFragment extends Fragment {
+import static android.content.ContentValues.TAG;
+
+public class LoginWithFragment extends Fragment implements View.OnClickListener{
+
+    private static final int RC_SIGN_IN = 1;
+    private GoogleSignInClient googleSignInClient;
+    private static final String TAG = "LoginWithFragment";
 
     public LoginWithFragment() {
         // Required empty public constructor
@@ -19,13 +38,69 @@ public class LoginWithFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleSignInClient = GoogleSignIn.getClient(Objects.requireNonNull(getContext()),gso);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login_with, container, false);
+
+        //Hooks
+        MaterialCardView loginWithGmailButton = view.findViewById(R.id.button_login_with_gmail);
+
+
+        //Click listeners
+        loginWithGmailButton.setOnClickListener(this);
+
         // Inflate the layout for this fragment
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d(TAG,"request code = "+resultCode);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.d(TAG, "signInResult:success =" + account.getFamilyName());
+            // Signed in successfully, show authenticated UI.
+//            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d(TAG, "signInResult:failed code=" + e.getStatusCode());
+//            updateUI(null);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.button_login_with_gmail)
+            signIn();
     }
 }
