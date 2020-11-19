89c4ff5764b4aa885432afa1a9e03e6f6c32c067
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-11-19 16:20:32 +0600

    fragment_onboarding_user_contact.xml | ui done

--- app/src/main/java/hn/techcom/com/hnapp/Fragments/OnboardingUserContactFragment.java
+++ app/src/main/java/hn/techcom/com/hnapp/Fragments/OnboardingUserContactFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.Random;
 
 import hn.techcom.com.hnapp.R;
 
@@ -21,6 +24,21 @@ public class OnboardingUserContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_onboarding_user_contact, container, false);
+
+        LinearLayout phoneLayout = view.findViewById(R.id.layout_phone_onboarding_contact);
+        LinearLayout emailLayout = view.findViewById(R.id.layout_email_onboarding_contact);
+
+        // this will later be replaced by user's sign in method
+        Random r = new Random();
+        int layoutToDisplay = r.nextInt(2);
+
+        switch(layoutToDisplay){
+            case 0:
+                phoneLayout.setVisibility(View.VISIBLE);
+                break;
+            default:
+                emailLayout.setVisibility(View.VISIBLE);
+        }
 
         // Inflate the layout for this fragment
         return view;
