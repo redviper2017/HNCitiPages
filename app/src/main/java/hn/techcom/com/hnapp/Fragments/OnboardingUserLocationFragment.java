bed0f3338f9007e79cf8453501944cf81023c3b2
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-11-21 18:11:40 +0600

    OnboardingUserLocationFragment.java | current location shown on map done!

--- app/src/main/java/hn/techcom/com/hnapp/Fragments/OnboardingUserLocationFragment.java
+++ app/src/main/java/hn/techcom/com/hnapp/Fragments/OnboardingUserLocationFragment.java
@@ -1,31 +1,50 @@
 package hn.techcom.com.hnapp.Fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.textview.MaterialTextView;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 import hn.techcom.com.hnapp.R;
 
-public class OnboardingUserLocationFragment extends Fragment{
+public class OnboardingUserLocationFragment extends Fragment implements View.OnClickListener {
 
-    Location currentLocation;
-    FusedLocationProviderClient fusedLocationProviderClient;
-    private static final int REQUEST_CODE = 101;
+    private MapView mMapView;
+    private GoogleMap googleMap;
+    private MaterialTextView city, country;
+    private MaterialCardView getCurrentLocationButton;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
-    private GoogleMap mMap;
-    private SupportMapFragment mapFragment;
+    double latitude, longitude;
 
     public OnboardingUserLocationFragment() {
         // Required empty public constructor
@@ -36,12 +55,126 @@ public class OnboardingUserLocationFragment extends Fragment{
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_onboarding_user_location, container, false);
 
+        //Hooks
+        city = view.findViewById(R.id.city_onboarding);
+        country = view.findViewById(R.id.country_onboarding);
+        getCurrentLocationButton = view.findViewById(R.id.button_get_current_location);
+        mMapView = view.findViewById(R.id.myMap);
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
 
+        //Initialise fused location
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(Objects.requireNonNull(getContext()));
+
+        //Click listeners
+        getCurrentLocationButton.setOnClickListener(this);
 
         // Inflate the layout for this fragment
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getActivity()), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                //Initialize location
+                Location location = task.getResult();
+                if (location != null) {
+                    try {
+                        //Initialize geoCoder
+                        Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                        //Initialize address list
+                        List<Address> address = geocoder.getFromLocation(
+                                location.getLatitude(), location.getLongitude(), 1
+                        );
+                        //Set latitude and longitude
+                        latitude = address.get(0).getLatitude();
+                        longitude = address.get(0).getLongitude();
+
+                        //Set city and country
+                        city.setText(address.get(0).getLocality());
+                        country.setText(address.get(0).getCountryName());
+
+                        setLocationOnMap();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void setLocationOnMap() {
+        MapsInitializer.initialize(Objects.requireNonNull(getActivity()).getApplicationContext());
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap map) {
+                googleMap = map;
+                if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+                LatLng currentLocation = new LatLng(latitude, longitude);
+                googleMap.addMarker(new MarkerOptions().position(currentLocation).title("Your Location").snippet("This is the location that we'll be using as your primary location for HN CitiPages."));
+                // For zooming functionality
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(currentLocation).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button_get_current_location) {
+            getLocation();
+            getCurrentLocationButton.setVisibility(View.GONE);
+        }
+    }
 }
