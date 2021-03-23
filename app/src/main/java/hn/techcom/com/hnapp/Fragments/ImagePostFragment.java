bc70b8579a70b43da3de8de3da33fecc4c4435cc
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-03-23 15:32:58 +0600

    camera permissions added in ImagePostFragment

--- app/src/main/java/hn/techcom/com/hnapp/Fragments/ImagePostFragment.java
+++ app/src/main/java/hn/techcom/com/hnapp/Fragments/ImagePostFragment.java
@@ -30,32 +30,32 @@ import com.kroegerama.imgpicker.ButtonType;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import hn.techcom.com.hnapp.R;
+import info.isuru.sheriff.enums.SheriffPermission;
+import info.isuru.sheriff.helper.Sheriff;
+import info.isuru.sheriff.interfaces.PermissionListener;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.content.ContentValues.TAG;
 
-public class ImagePostFragment extends Fragment implements View.OnClickListener, BottomSheetImagePicker.OnImagesSelectedListener {
+public class ImagePostFragment extends Fragment implements View.OnClickListener, BottomSheetImagePicker.OnImagesSelectedListener, PermissionListener {
 
 
+    private static final int REQUEST_SINGLE_PERMISSION = 1;
+    private CardView selectImageButton, captureImageButton;
 
-    private View view;
-
-    private CardView selectImageButton;
-
-    String permissions[] = {"android.permission.CAMERA", "android.permission.READ_EXTERNAL_STORAGE"};
-    int PERMISSION_REQUEST_CODE = 200;
+    private  Sheriff sheriffPermission;
 
 
     public ImagePostFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -70,132 +70,32 @@ public class ImagePostFragment extends Fragment implements View.OnClickListener,
 
 
         selectImageButton = view.findViewById(R.id.select_image_button);
+        captureImageButton = view.findViewById(R.id.capture_image_button);
 
         ((CardView) selectImageButton).setOnClickListener(this);
+        ((CardView) captureImageButton).setOnClickListener(this);
 
-//        Spinner spinner = view.findViewById(R.id.spinner_post_type);
-//
-//        LinearLayout uploadFromFileButton = view.findViewById(R.id.button_upload_from_gallery);
-//
-//        LinearLayout uploadFromCameraButton = view.findViewById(R.id.button_upload_from_camera);
-
-//        String[] arrayPostType = new String[]{"Random",
-//                "Positive Thoughts",
-//                "Talent",
-//                "Culture",
-//                "News",
-//                "Emergency"};
-
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
-//                android.R.layout.simple_spinner_dropdown_item, arrayPostType);
-//        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-//
-//        uploadFromFileButton.setOnClickListener(this);
-//
-//        uploadFromCameraButton.setOnClickListener(this);
-
-//        getLatestCameraImage();
+        //Sherif permission object
+        sheriffPermission = Sheriff.Builder()
+                .with(this)
+                .requestCode(REQUEST_SINGLE_PERMISSION)
+                .setPermissionResultCallback(this)
+                .askFor(SheriffPermission.CAMERA)
+                .build();
 
         // Inflate the layout for this fragment
         return view;
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        switch (requestCode) {
-//            case 200:
-//                if (grantResults.length > 0) {
-//                    boolean storageAccessAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-//                    boolean cameraAccessAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-//
-//                    if (storageAccessAccepted && cameraAccessAccepted) {
-////                        getLatestCameraImage();
-//                    }
-//                    else {
-//                        Snackbar.make(view, "Permission Denied, You cannot access location data and camera.", Snackbar.LENGTH_LONG).show();
-//                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                            if (shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE)) {
-//                                showMessageOKCancel("To use this feature you need to allow access to both the permissions",
-//                                        new DialogInterface.OnClickListener() {
-//                                            @Override
-//                                            public void onClick(DialogInterface dialog, int which) {
-//                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                                                    requestPermissions(new String[]{READ_EXTERNAL_STORAGE, CAMERA},
-//                                                            PERMISSION_REQUEST_CODE);
-//                                                }
-//                                            }
-//                                        });
-//                            }
-//                        }
-//                    }
-//                }
-//                break;
-//        }
-    }
-
-//    public void getLatestCameraImage() {
-//        if (checkPermission()) {
-//            File dcimPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + "/Camera");
-//            if (dcimPath.exists()) {
-//                Log.i("DCIM PATH", dcimPath.toString());
-//
-//                File[] files = dcimPath.listFiles();
-//                Arrays.sort(files, new Comparator<File>() {
-//                    public int compare(File f1, File f2) {
-//                        return Long.compare(f1.lastModified(), f2.lastModified());
-//                    }
-//                });
-//                Picasso.get()
-//                        .load(files[files.length - 2])
-//                        .into(galleryPreview);
-//
-//                Log.d("ImagePostFragment", "first file path = " + files[files.length - 2].getName());
-//            }
-//        } else {
-//            requestPermission();
-//        }
-//    }
-
-    private boolean checkPermission() {
-        int result = ContextCompat.checkSelfPermission(getContext(), READ_EXTERNAL_STORAGE);
-        int result1 = ContextCompat.checkSelfPermission(getContext(), CAMERA);
-
-        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestPermission() {
-        ActivityCompat.requestPermissions(getActivity(), new String[]{READ_EXTERNAL_STORAGE, CAMERA}, PERMISSION_REQUEST_CODE);
-    }
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(getContext())
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
-    }
-
-    public void getFromGallery() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
-    }
-
-    @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.button_upload_from_gallery) {
+        if (v.getId() == R.id.capture_image_button) {
 
-//            getFromGallery();
-
-            Toast.makeText(getContext(),"opening gallery app!",Toast.LENGTH_LONG).show();
+            Toast.makeText(getContext(), "opening gallery app!", Toast.LENGTH_LONG).show();
+            sheriffPermission.requestPermissions();
 
         } else if (v.getId() == R.id.select_image_button) {
-            Toast.makeText(getContext(),"opening camera app!",Toast.LENGTH_LONG).show();
+            Toast.makeText(getContext(), "opening camera app!", Toast.LENGTH_LONG).show();
             new BottomSheetImagePicker.Builder(getString(R.string.file_provider))
                     .multiSelect(1, 4)
                     .multiSelectTitles(
@@ -212,8 +112,18 @@ public class ImagePostFragment extends Fragment implements View.OnClickListener,
 
     @Override
     public void onImagesSelected(@NonNull List<? extends Uri> uris, @Nullable String s) {
-        for(Uri uri: uris){
-            Log.d(TAG,"selected image uri = "+uri);
+        for (Uri uri : uris) {
+            Log.d(TAG, "selected image uri = " + uri);
         }
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, ArrayList<String> acceptedPermissionList) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, ArrayList<String> deniedPermissionList) {
+
     }
 }
