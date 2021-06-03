4c1fde1ddcf70b5bd50071a3ca8b99fcb49c3628
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-06-03 09:09:29 +0600

    PostImageActivity - display capture image bug resolved in newer android versions

--- app/src/main/java/hn/techcom/com/hnapp/Activities/PostImageActivity.java
+++ app/src/main/java/hn/techcom/com/hnapp/Activities/PostImageActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.DialogInterface;
@@ -16,6 +17,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.text.TextUtils;
@@ -37,6 +39,7 @@ import com.kroegerama.imgpicker.BottomSheetImagePicker;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -82,6 +85,7 @@ public class PostImageActivity extends AppCompatActivity implements View.OnClick
     private TextInputEditText imageCaption;
     private File newImageFile;
     private String newImageFileName, newImageAspectRatio;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -199,9 +203,9 @@ public class PostImageActivity extends AppCompatActivity implements View.OnClick
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            imageview.setImageURI(Uri.fromFile(new File(newImageFileName)));
+            imageview.setImageURI(Uri.fromFile(new File(currentPhotoPath)));
 
-            String imageAspect = getImageDimension(newImageFileName);
+            String imageAspect = getImageDimension(currentPhotoPath);
             Log.d(TAG,"aspect of image = "+imageAspect);
 
             newImageAspectRatio = imageAspect;
@@ -283,21 +287,43 @@ public class PostImageActivity extends AppCompatActivity implements View.OnClick
     }
 
     private void startImageCaptureIntent(){
-        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-        StrictMode.setVmPolicy(builder.build());
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(this.getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "hn.techcom.com.hnapp.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
 
-        Intent inent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = this.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
-        Date date = new Date();
-        DateFormat df = new SimpleDateFormat("-mm-ss");
-        String newImageName = df.format(date) + ".jpg";
-        String newImagePath = "/sdcard/" + newImageName;
-        newImageFile = new File(newImagePath);
-        newImageFileName = newImageFile.toString();
-        Uri outuri = Uri.fromFile(newImageFile);
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
 
-        inent.putExtra(MediaStore.EXTRA_OUTPUT, outuri);
-        startActivityForResult(inent, REQUEST_IMAGE_CAPTURE);
+        newImageFile = image;
+        return image;
     }
 
     public void startImagePickIntent(){
