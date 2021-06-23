c41b88f5553b1a2b1773122109ba154cf1ce5944
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-06-23 13:30:40 +0600

    Audio post view added - PostListAdapter

--- app/src/main/java/hn/techcom/com/hnapp/Adapters/PostListAdapter.java
+++ app/src/main/java/hn/techcom/com/hnapp/Adapters/PostListAdapter.java
@@ -1,17 +1,13 @@
 package hn.techcom.com.hnapp.Adapters;
 
 import android.content.Context;
-import android.media.MediaPlayer;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.SeekBar;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,7 +18,6 @@ import com.potyvideo.library.AndExoPlayerView;
 import com.santalu.aspectratioimageview.AspectRatioImageView;
 import com.squareup.picasso.Picasso;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -113,13 +108,10 @@ public class PostListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
                         .inflate(R.layout.row_image_post, parent, false);
                 return new ImageViewHolder(view);
             case VIEW_TYPE_VIDEO:
-                view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.row_video_post, parent, false);
-                return new VideoViewHolder(view);
             case VIEW_TYPE_AUDIO:
                 view = LayoutInflater.from(parent.getContext())
-                        .inflate(R.layout.row_audiio_post, parent, false);
-                return new AudioViewHolder(view);
+                        .inflate(R.layout.row_video_post, parent, false);
+                return new AudioVideoViewHolder(view);
             case VIEW_TYPE_LOADING:
                 view = LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.row_loading_post, parent, false);
@@ -139,10 +131,8 @@ public class PostListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
                 ((ImageViewHolder) holder).bind(post);
                 break;
             case VIEW_TYPE_VIDEO:
-                ((VideoViewHolder) holder).bind(post);
-                break;
             case VIEW_TYPE_AUDIO:
-                ((AudioViewHolder) holder).bind(post);
+                ((AudioVideoViewHolder) holder).bind(post);
                 break;
             case VIEW_TYPE_LOADING:
                 ((LoadingViewHolder) holder).bind();
@@ -504,14 +494,14 @@ public class PostListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     }
 
     //Video view holder class
-    private class VideoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    private class AudioVideoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         public MaterialTextView name, location, text, likes, comments, seeMoreButton;
         public CircleImageView avatar;
         private AndExoPlayerView videoPlayerPortrait, videoPlayerLandscape;
         private ImageButton optionsButton, likeButton, favoriteButton, commentButton;;
 
-        public VideoViewHolder(@NonNull View view) {
+        public AudioVideoViewHolder(@NonNull View view) {
             super(view);
 
             name                 = view.findViewById(R.id.name_post);
@@ -621,20 +611,27 @@ public class PostListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 //
 //            portraitVideoView.setVideoPath(videoUrl);
 //            portraitVideoView.requestFocus();
-            if (post.getFiles().get(0).getAspect().equals("portrait")){
-                videoPlayerPortrait.setVisibility(View.VISIBLE);
-                videoPlayerLandscape.setVisibility(View.GONE);
-                videoPlayerPortrait.setSource(videoUrl);
-                videoPlayerPortrait.setPlayWhenReady(false);
-//                videoPlayerPortrait.stopPlayer();
-//                videoPlayerPortrait.seekForward(1);
-            }else{
+            if(post.getPosttype().equals("A")){
                 videoPlayerLandscape.setVisibility(View.VISIBLE);
                 videoPlayerPortrait.setVisibility(View.GONE);
                 videoPlayerLandscape.setSource(videoUrl);
                 videoPlayerLandscape.setPlayWhenReady(false);
+            }else{
+                if (post.getFiles().get(0).getAspect().equals("portrait")){
+                    videoPlayerPortrait.setVisibility(View.VISIBLE);
+                    videoPlayerLandscape.setVisibility(View.GONE);
+                    videoPlayerPortrait.setSource(videoUrl);
+                    videoPlayerPortrait.setPlayWhenReady(false);
+//                videoPlayerPortrait.stopPlayer();
+//                videoPlayerPortrait.seekForward(1);
+                }else{
+                    videoPlayerLandscape.setVisibility(View.VISIBLE);
+                    videoPlayerPortrait.setVisibility(View.GONE);
+                    videoPlayerLandscape.setSource(videoUrl);
+                    videoPlayerLandscape.setPlayWhenReady(false);
 //                videoPlayerLandscape.stopPlayer();
 //                videoPlayerLandscape.seekForward(1);
+                }
             }
         }
 
@@ -654,211 +651,6 @@ public class PostListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
                 onLikeCountButtonListener.onLikeCountButtonClick(postId);
             if(view.getId() == R.id.comment_button_post || view.getId() == R.id.text_comment_count_post)
                 onCommentClickListener.onCommentClick(postId);
-        }
-    }
-
-    //Audio view holder class
-    private class AudioViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-
-        public MaterialTextView name, location, text, likes, comments, seeMoreButton, currentTime, totalTime;
-        public CircleImageView avatar;
-        public ImageView playPauseButton;
-        public SeekBar seekbar;
-        private ImageButton optionsButton, likeButton, favoriteButton, commentButton;
-        public MediaPlayer mediaPlayer;
-        public Handler handler = new Handler();
-
-        public AudioViewHolder(@NonNull View view) {
-            super(view);
-
-            name                 = view.findViewById(R.id.name_post);
-            location             = view.findViewById(R.id.location_post);
-            text                 = view.findViewById(R.id.text_post);
-            likes                = view.findViewById(R.id.text_like_count_post);
-            comments             = view.findViewById(R.id.text_comment_count_post);
-            avatar               = view.findViewById(R.id.avatar_post);
-            seeMoreButton        = view.findViewById(R.id.seemore_post);
-            currentTime          = view.findViewById(R.id.currentTime_text);
-            totalTime            = view.findViewById(R.id.totalTime_text);
-            playPauseButton      = view.findViewById(R.id.playPause_imagebutton_audio);
-            seekbar              = view.findViewById(R.id.seekbar_audio);
-            optionsButton        = view.findViewById(R.id.options_icon_post);
-            likeButton           = view.findViewById(R.id.like_button_post);
-            favoriteButton       = view.findViewById(R.id.favorite_button_post);
-            commentButton        = view.findViewById(R.id.comment_button_post);
-
-            mediaPlayer          = new MediaPlayer();
-            seekbar.setMax(100);
-
-            optionsButton.setOnClickListener(this);
-            likeButton.setOnClickListener(this);
-            favoriteButton.setOnClickListener(this);
-            likes.setOnClickListener(this);
-            comments.setOnClickListener(this);
-            commentButton.setOnClickListener(this);
-
-            playPauseButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view) {
-                    if(mediaPlayer.isPlaying()){
-                        handler.removeCallbacks(updater);
-                        mediaPlayer.pause();
-                        playPauseButton.setImageResource(R.drawable.play_button_ic);
-                    }else{
-                        mediaPlayer.start();
-                        playPauseButton.setImageResource(R.drawable.pause_button_ic);
-                        updateSeekBar();
-                    }
-                }
-            });
-
-            seekbar.setOnTouchListener(new View.OnTouchListener(){
-                @Override
-                public boolean onTouch(View view, MotionEvent motionEvent) {
-                    SeekBar seekBar = (SeekBar) view;
-                    int playPosition = (mediaPlayer.getDuration() / 100) * seekbar.getProgress();
-                    mediaPlayer.seekTo(playPosition);
-                    currentTime.setText(milliSecondsToTimer(mediaPlayer.getCurrentPosition()));
-                    return false;
-                }
-            });
-        }
-        void bind(Result post){
-            String address = post.getUser().getCity() + ", " + post.getUser().getCountry();
-
-            name.setText(post.getUser().getFullName());
-            location.setText(address);
-            text.setText(post.getText());
-
-            //see more button toggle for large texts
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    if (text.getLayout() != null) {
-                        Log.d(TAG, "line count = " + text.getLayout().getLineCount());
-                        if (text.getLayout().getLineCount() > 9)
-                            seeMoreButton.setVisibility(View.VISIBLE);
-                        else
-                            seeMoreButton.setVisibility(View.GONE);
-                    }
-                }
-            },100);
-
-            //Toggling like button
-            if(post.getLiked())
-                likeButton.setImageResource(R.drawable.filled_thumb_up_24_ic);
-            else
-                likeButton.setImageResource(R.drawable.outline_thumb_up_24_ic);
-
-            //Toggling favorite button
-            if(post.getFavourite())
-                favoriteButton.setImageResource(R.drawable.favorite_ic_selected);
-            else
-                favoriteButton.setImageResource(R.drawable.outline_favorite_24_ic);
-
-            Log.d(TAG, "favorite = "+post.getFavourite().toString());
-
-
-            //Toggling comment count text
-            if (post.getCommentCount() != 0){
-                comments.setVisibility(View.VISIBLE);
-                if(post.getCommentCount() >1) {
-                    String commentText = post.getCommentCount() + " comments";
-                    comments.setText(commentText);
-                }else{
-                    String commentText = post.getCommentCount() + " comment";
-                    comments.setText(commentText);
-                }
-            }else
-                comments.setVisibility(View.GONE);
-
-            //Toggling like count text
-            if(post.getLikeCount() != 0){
-                likes.setVisibility(View.VISIBLE);
-                if(post.getLikeCount() >1) {
-                    String likeText = post.getLikeCount() + " likes";
-                    likes.setText(likeText);
-                }else{
-                    String likeText = post.getLikeCount() + " like";
-                    likes.setText(likeText);
-                }
-            }else
-                likes.setVisibility(View.GONE);
-
-            String profilePhotoUrl = post.getUser().getProfileImg();
-            Picasso
-                    .get()
-                    .load(profilePhotoUrl)
-                    .into(avatar);
-
-
-            prepareMediaPlayer(post);
-        }
-
-        @Override
-        public void onClick(View view) {
-            int position = getAbsoluteAdapterPosition();
-            int postId   = allPosts.get(position).getId();
-            String hnid_user = allPosts.get(position).getUser().getHnid();
-            boolean supporting = allPosts.get(position).getUser().getIsSupported();
-            if(view.getId() == R.id.options_icon_post)
-                onOptionsButtonClickListener.onOptionsButtonClick(position, postId, hnid_user, supporting);
-            if(view.getId() == R.id.like_button_post)
-                onLikeButtonClickListener.onLikeButtonClick(position, postId);
-            if(view.getId() == R.id.favorite_button_post)
-                onFavoriteButtonClickListener.onFavoriteButtonClick(position, postId);
-            if(view.getId() == R.id.text_like_count_post)
-                onLikeCountButtonListener.onLikeCountButtonClick(postId);
-            if(view.getId() == R.id.comment_button_post || view.getId() == R.id.text_comment_count_post)
-                onCommentClickListener.onCommentClick(postId);
-        }
-
-        private void prepareMediaPlayer(Result post){
-            mediaPlayer          = new MediaPlayer();
-            try{
-                String audioUrl = post.getFiles().get(0).getMedia();
-                mediaPlayer.setDataSource(audioUrl);  //url of audio file
-                mediaPlayer.prepare();
-                totalTime.setText(milliSecondsToTimer(mediaPlayer.getDuration()));
-            } catch (IOException e) {
-                Log.d(TAG,"audio load exception message = "+e.getMessage());
-            }
-        }
-
-        private Runnable updater = new Runnable() {
-            @Override
-            public void run() {
-                updateSeekBar();
-                long currentDuration = mediaPlayer.getCurrentPosition();
-                currentTime.setText(milliSecondsToTimer(currentDuration));
-            }
-        };
-
-        private void updateSeekBar(){
-            if(mediaPlayer.isPlaying()) {
-                seekbar.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration()) * 100));
-                handler.postDelayed(updater, 1000);
-            }
-        }
-
-        private String milliSecondsToTimer(long milliseconds){
-            String timerString = "";
-            String secondsString;
-
-            int hours   = (int) (milliseconds / (1000 * 60 * 60));
-            int minutes = (int) (milliseconds % (1000 * 60 * 60)) / (1000 * 60);
-            int seconds = (int) ((milliseconds % (1000 * 60 * 60)) % (1000 * 60) / 1000);
-
-            if (hours > 0)
-                timerString = hours + ":";
-            if (seconds < 10)
-                secondsString = "0" + seconds;
-            else
-                secondsString = "" + seconds;
-
-            timerString = timerString + minutes + ":" + secondsString;
-            return timerString;
         }
     }
 
