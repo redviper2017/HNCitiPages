784d2d43e5f5a962cbeccd1406cc72adbc02f6be
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-07-04 20:15:11 +0600

    Permission bug resolved - MainActivity.java

--- app/src/main/java/hn/techcom/com/hnapp/Activities/MainActivity.java
+++ app/src/main/java/hn/techcom/com/hnapp/Activities/MainActivity.java
@@ -1,10 +1,16 @@
 package hn.techcom.com.hnapp.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,11 +36,16 @@ import hn.techcom.com.hnapp.R;
 import hn.techcom.com.hnapp.Utils.BottomSheetFragment;
 import hn.techcom.com.hnapp.Utils.Utils;
 
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class MainActivity extends AppCompatActivity {
 
     static ArrayList<SupporterProfile> userSupportedProfiles;
     static ArrayList<Post> globalPosts = new ArrayList<>();
     static ArrayList<Post> userSupportedProfilePosts = new ArrayList<>();
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     //currently its hard coded but later on it will taken from local db based on currently logged in user's username
     private String currentUserUsername = "redviper";
@@ -57,8 +68,12 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, SignInActivity.class));
         }
         else {
-            Fragment fragment = new HomeFragment();
-            getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_main, Objects.requireNonNull(fragment)).commit();
+            if (checkPermission()) {
+                Fragment fragment = new HomeFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_main, Objects.requireNonNull(fragment)).commit();
+            }else{
+                requestPermission();
+            }
         }
     }
 
@@ -112,5 +127,64 @@ public class MainActivity extends AppCompatActivity {
         if(fragmentSelected != null)
             getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_main, Objects.requireNonNull(fragmentSelected)).commit();
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0){
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean recordAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (storageAccepted && recordAccepted){
+                        Fragment fragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_main, Objects.requireNonNull(fragment)).commit();
+                    }
+                    else
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) {
+                            showMessageOKCancel(
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA, RECORD_AUDIO},
+                                                    PERMISSION_REQUEST_CODE);
+                                        }
+                                    });
+                            return;
+                        }
+                    }
+                }else
+                    finish();
+        }
+    }
+
+    //Custom methods
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(this, CAMERA);
+        int result2 = ContextCompat.checkSelfPermission(this, RECORD_AUDIO);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, CAMERA, RECORD_AUDIO}, PERMISSION_REQUEST_CODE);
+    }
+
+    private void showMessageOKCancel(DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage("You need to allow access to all of these permission in order to continue with this app.")
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                        onDestroy();
+                    }
+                })
+                .create()
+                .show();
     }
 }
