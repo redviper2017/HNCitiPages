3d61108746f3bf61ea9882aae4d484387dac1344
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-07-04 20:37:11 +0600

    Added permissions - SignInActivity

--- app/src/main/java/hn/techcom/com/hnapp/Activities/SignInActivity.java
+++ app/src/main/java/hn/techcom/com/hnapp/Activities/SignInActivity.java
@@ -1,12 +1,19 @@
 package hn.techcom.com.hnapp.Activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -36,6 +43,7 @@ import com.hbb20.CountryCodePicker;
 
 import java.util.Objects;
 
+import hn.techcom.com.hnapp.Fragments.HomeFragment;
 import hn.techcom.com.hnapp.Interfaces.GetDataService;
 import hn.techcom.com.hnapp.Models.Profile;
 import hn.techcom.com.hnapp.Models.ValidationResponse;
@@ -44,6 +52,10 @@ import hn.techcom.com.hnapp.R;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -58,6 +70,15 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private static final int RC_SIGN_IN = 1;
     private FirebaseAuth mAuth;
     private Profile newUser;
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!checkPermission()) {
+            requestPermission();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -145,6 +166,37 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         if(requestCode == RC_SIGN_IN){
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0){
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean recordAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (storageAccepted && recordAccepted){
+                        Fragment fragment = new HomeFragment();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_main, Objects.requireNonNull(fragment)).commit();
+                    }
+                    else
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) {
+                            showMessageOKCancel(
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA, RECORD_AUDIO},
+                                                    PERMISSION_REQUEST_CODE);
+                                        }
+                                    });
+                            return;
+                        }
+                    }
+                }else
+                    finish();
         }
     }
 
@@ -298,4 +350,31 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    //Custom methods
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(this, CAMERA);
+        int result2 = ContextCompat.checkSelfPermission(this, RECORD_AUDIO);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, CAMERA, RECORD_AUDIO}, PERMISSION_REQUEST_CODE);
+    }
+
+    private void showMessageOKCancel(DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage("You need to allow access to all of these permission in order to continue with this app.")
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                        onDestroy();
+                    }
+                })
+                .create()
+                .show();
+    }
 }
