8a6a00a0ff44e4a1085f2bd99b66ce676e3a15b7
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-07-27 04:13:03 +0600

    PostsActivity - initial post list added

--- app/src/main/java/hn/techcom/com/hnapp/Activities/PostsActivity.java
+++ app/src/main/java/hn/techcom/com/hnapp/Activities/PostsActivity.java
@@ -1,26 +1,83 @@
 package hn.techcom.com.hnapp.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.material.textview.MaterialTextView;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import hn.techcom.com.hnapp.Adapters.PostListAdapter;
+import hn.techcom.com.hnapp.Adapters.ProfileListAdapter;
+import hn.techcom.com.hnapp.Fragments.InteractWithPostBottomSheetFragment;
+import hn.techcom.com.hnapp.Interfaces.GetDataService;
+import hn.techcom.com.hnapp.Interfaces.OnCommentClickListener;
+import hn.techcom.com.hnapp.Interfaces.OnFavoriteButtonClickListener;
+import hn.techcom.com.hnapp.Interfaces.OnLikeButtonClickListener;
+import hn.techcom.com.hnapp.Interfaces.OnLikeCountButtonListener;
+import hn.techcom.com.hnapp.Interfaces.OnLoadMoreListener;
+import hn.techcom.com.hnapp.Interfaces.OnOptionsButtonClickListener;
+import hn.techcom.com.hnapp.Models.FavoriteResponse;
+import hn.techcom.com.hnapp.Models.LikeResponse;
+import hn.techcom.com.hnapp.Models.Profile;
+import hn.techcom.com.hnapp.Models.Result;
+import hn.techcom.com.hnapp.Network.RetrofitClientInstance;
 import hn.techcom.com.hnapp.R;
+import hn.techcom.com.hnapp.Utils.Utils;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class PostsActivity extends AppCompatActivity implements View.OnClickListener {
+public class PostsActivity
+        extends AppCompatActivity
+        implements
+        View.OnClickListener,
+        OnOptionsButtonClickListener,
+        OnLikeButtonClickListener,
+        OnFavoriteButtonClickListener,
+        OnLikeCountButtonListener,
+        OnCommentClickListener,
+        OnLoadMoreListener {
+
+    private RecyclerView recyclerView;
+    private PostListAdapter adapter;
+    private Utils myUtils;
+    private Profile userProfile;
+    private ArrayList<Result> recentPostList;
+    private static final String TAG = "PostsActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
 
+        myUtils = new Utils();
+        userProfile = myUtils.getNewUserFromSharedPreference(this);
+        recentPostList = getRecentPosts();
+
         //Hooks
         ImageButton backButton       = findViewById(R.id.image_button_back);
         MaterialTextView screenTitle = findViewById(R.id.text_screen_title);
         MaterialTextView countText   = findViewById(R.id.count_text);
+        recyclerView                 = findViewById(R.id.recyclerview);
+
+        setRecyclerView(recentPostList);
 
         String count = getIntent().getStringExtra("PostCount");
         countText.setText(count);
@@ -31,6 +88,18 @@ public class PostsActivity extends AppCompatActivity implements View.OnClickList
 
         //OnClick Listeners
         backButton.setOnClickListener(this);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
     }
 
     @Override
@@ -38,4 +107,130 @@ public class PostsActivity extends AppCompatActivity implements View.OnClickList
         if(v.getId() == R.id.image_button_back)
             super.onBackPressed();
     }
+
+    private ArrayList<Result> getRecentPosts(){
+        SharedPreferences sharedPreferences = this.getSharedPreferences("MY_PREF", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("RecentPosts", null);
+        Type type = new TypeToken<ArrayList<Result>>() {}.getType();
+        return gson.fromJson(json, type);
+    }
+
+    public void setRecyclerView(ArrayList<Result> postList){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new PostListAdapter(
+                postList,
+                this,
+                this,
+                this,
+                this,
+                this,
+                this,
+                this);
+        recyclerView.setAdapter(adapter);
+//        swipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onCommentClick(int postId) {
+        Intent intent = new Intent(this, ViewCommentsActivity.class);
+        intent.putExtra("POST_ID",postId);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onFavoriteButtonClick(int position, int postId) {
+        favoriteOrUnfavoritePost(userProfile.getHnid(), postId, position);
+    }
+
+    @Override
+    public void onLikeButtonClick(int position, int postId) {
+        likeOrUnlikeThisPost(userProfile.getHnid(), postId, position);
+    }
+
+    @Override
+    public void onLikeCountButtonClick(int postId) {
+        Intent intent = new Intent(this, ViewLikesActivity.class);
+        intent.putExtra("POST_ID",postId);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onLoadMore() {
+
+    }
+
+    @Override
+    public void onOptionsButtonClick(int position, int postId, String hnid_user, boolean supporting) {
+        InteractWithPostBottomSheetFragment interactWithPostBottomSheetFragment = new InteractWithPostBottomSheetFragment(position, postId, recentPostList, adapter, hnid_user, supporting);
+        interactWithPostBottomSheetFragment.show(getSupportFragmentManager(), interactWithPostBottomSheetFragment.getTag());
+    }
+
+    //like or un-like post
+    public void likeOrUnlikeThisPost(String hnid, int postId, int position){
+        RequestBody user = RequestBody.create(MediaType.parse("text/plain"), hnid);
+        RequestBody post = RequestBody.create(MediaType.parse("text/plain"), String.valueOf(postId));
+
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<LikeResponse> call = service.likeOrUnlikePost(user,post);
+
+        call.enqueue(new Callback<LikeResponse>() {
+            @Override
+            public void onResponse(Call<LikeResponse> call, Response<LikeResponse> response) {
+                if(response.code() == 201){
+                    LikeResponse likeResponse = response.body();
+                    Toast.makeText(PostsActivity.this, likeResponse.getMessage(), Toast.LENGTH_LONG).show();
+
+                    //Toggling like button image
+                    recentPostList.get(position).setLiked(!recentPostList.get(position).getLiked());
+
+                    //Toggling like count on post
+                    if(recentPostList.get(position).getLiked())
+                        recentPostList.get(position).setLikeCount(recentPostList.get(position).getLikeCount() + 1);
+                    else
+                        recentPostList.get(position).setLikeCount(recentPostList.get(position).getLikeCount() - 1);
+
+                    adapter.notifyDataSetChanged();
+                }else
+                    Toast.makeText(PostsActivity.this, "Sorry unable to like the post at this moment, try again later.", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<LikeResponse> call, Throwable t) {
+                Toast.makeText(PostsActivity.this, "Your request has been failed! Please check your internet connection.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+    //favorite or un-favorite post
+    public void favoriteOrUnfavoritePost(String hnid, int postId, int position){
+        RequestBody user = RequestBody.create(MediaType.parse("text/plain"), hnid);
+        RequestBody post = RequestBody.create(MediaType.parse("text/plain"), String.valueOf(postId));
+
+        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
+        Call<FavoriteResponse> call = service.favoriteOrUnfavoritePost(user,post);
+
+        call.enqueue(new Callback<FavoriteResponse>() {
+            @Override
+            public void onResponse(Call<FavoriteResponse> call, Response<FavoriteResponse> response) {
+                if(response.code() == 201){
+                    FavoriteResponse favoriteResponse = response.body();
+                    Toast.makeText(PostsActivity.this, favoriteResponse.getMessage(), Toast.LENGTH_LONG).show();
+
+                    //Toggling favorite button image
+                    recentPostList.get(position).setFavourite(!recentPostList.get(position).getFavourite());
+
+                    adapter.notifyDataSetChanged();
+                }else
+                    Toast.makeText(PostsActivity.this, "Sorry unable to like the post at this moment, try again later.", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<FavoriteResponse> call, Throwable t) {
+                Toast.makeText(PostsActivity.this, "Your request has been failed! Please check your internet connection.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
