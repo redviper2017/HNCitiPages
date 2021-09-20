3ab770afdebd0c51aa1865e905ec97efefd55f9c
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-09-20 14:24:41 +0600

    Gradle version upgraded | jCenter() replaced by mavenCentral()

--- app/src/main/java/hn/techcom/com/hncitipages/Database/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package hn.techcom.com.hncitipages.Database;
-
-import androidx.room.RoomDatabase;
-
-import hn.techcom.com.hncitipages.Interfaces.UserProfileDao;
-
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract UserProfileDao userProfileDao();
-}
